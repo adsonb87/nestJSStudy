08305ff2752c70d4737fd4902aa0f68cb1eabab5
adsonb87 <adsonb87@gmail.com>
2024-01-08 19:16:09 -0300

    Implementação class-validator e class-transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,16 @@
+import { Exclude, Expose, Transform } from 'class-transformer';
+import { IsNotEmpty, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Nome não pode ser vazio !!!' })
+  @Length(3, 10, { message: 'Nome precisa ter entre 3 e 10 caracteres' })
+  //@Transform(({ value }) => value.toLowerCase())
+  //@Expose({ name: 'nome_completo' })
   nome: string;
+
+  @IsNotEmpty({ message: 'Idade não pode ser vazia !!!' })
   idade: number;
+
+  @Exclude()
+  id: number;
 }
